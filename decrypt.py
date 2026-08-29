01195505eb519075d38bf05a9e0db5b471d41503
Ana Sarah Nagata <anarachan@email.com>
2026-08-29 19:49:43 -0300

    refactor: unify decrypt.py and encrypt.py in vigenere_cypher.py and add documentation

--- decrypt.py
+++
@@ -1,9 +0,0 @@
-from math import ceil
-
-
-def decrypt(key: str, ciphertext: str) -> str:
-    full_key: str = ceil(len(ciphertext) / len(key)) * key
-    plaintext = ""
-    for key_character, text_character in zip(full_key, ciphertext):
-        plaintext += chr(ord(text_character) - ord(key_character))
-    return plaintext
